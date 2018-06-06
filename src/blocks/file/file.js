2176237da482931f373b261510ef7a5beb326c1f
Maxim Sofronov <imaxsof@gmail.com>
2018-06-07 00:41:05 +0300

    no message

--- src/blocks/file/file.js
+++ src/blocks/file/file.js
@@ -7,12 +7,13 @@ window.sedona.file = function() {
 
 		Array.prototype.forEach.call(element, function(el) {
 			if (!el.classList.contains('file--js-init')) {
-				el.classList.add('table--js-init', 'file--input');
+				el.classList.add('file--js-init');
+				el.classList.add('file--input');
 
 				let id = el.getAttribute('id');
 
 				let layout = '' +
-					'<label class="file" for="' + id + '">' +
+					'<label class="file  file--pseudo" for="' + id + '">' +
 						'<ul class="file__field"></ul>' +
 						'<div class="file__button"></div>' +
 					'</label>';
@@ -29,7 +30,32 @@ window.sedona.file = function() {
 						filesList = filesList + '<li>' + fileName + '</li>';
 					}
 
-					document.querySelector('.file[for="' + id + '"] .file__field').innerHTML = filesList;
+					document.querySelector('label.file[for="' + id + '"] .file__field').innerHTML = filesList;
+				});
+
+				let droppable = document.querySelector('label.file[for="' + id + '"]');
+
+				droppable.addEventListener('dragover', function(evt) {
+					evt.stopPropagation();
+					evt.preventDefault();
+					el.classList.add('file--droppable');
+				});
+
+				el.addEventListener('dragover', function(evt) {
+					evt.stopPropagation();
+					evt.preventDefault();
+					el.classList.add('file--droppable');
+				});
+
+				el.addEventListener('dragleave', function(evt) {
+					evt.stopPropagation();
+					evt.preventDefault();
+					el.classList.remove('file--droppable');
+				});
+
+				el.addEventListener('drop', function(evt) {
+					evt.stopPropagation();
+					el.classList.remove('file--droppable');
 				});
 			}
 		});
