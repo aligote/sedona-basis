5faca3c411d7ebcbe44e115e049f5734bfadd188
Maxim Sofronov <imaxsof@gmail.com>
2018-06-08 11:35:10 +0300

    no message

--- src/blocks/file/file.js
+++ src/blocks/file/file.js
@@ -22,15 +22,33 @@ window.sedona.file = function() {
 
 				el.addEventListener('change', function() {
 					let filesSelected = this.files;
+					let filesField = document.querySelector('label.file[for="' + id + '"] .file__field');
 					let filesList = '';
+					let filesPreview = 1;
 
 					for (let i = 0; i < filesSelected.length; i++) {
 						let fileName = filesSelected[i].name;
+						let fileType = filesSelected[i].type;
+						let filePreview = window.URL.createObjectURL(filesSelected[i]);
 
-						filesList = filesList + '<li>' + fileName + '</li>';
+						if (!/image\//i.test(fileType)) {
+							filesPreview = 0;
+						}
+
+						filesList = filesList +
+							'<li>' +
+								'<span class="file__preview" style="background-image: url(' + filePreview + ');"></span>' +
+								'<span class="file__name">' + fileName + '</span>' +
+							'</li>';
 					}
 
-					document.querySelector('label.file[for="' + id + '"] .file__field').innerHTML = filesList;
+					filesField.classList.remove('file__field--preview');
+
+					if (filesPreview) {
+						filesField.classList.add('file__field--preview');
+					}
+
+					filesField.innerHTML = filesList;
 				});
 
 				let droppable = document.querySelector('label.file[for="' + id + '"]');
@@ -38,26 +56,54 @@ window.sedona.file = function() {
 				droppable.addEventListener('dragover', function(evt) {
 					evt.stopPropagation();
 					evt.preventDefault();
+
 					el.classList.add('file--droppable');
 				});
 
 				el.addEventListener('dragover', function(evt) {
 					evt.stopPropagation();
 					evt.preventDefault();
+
 					el.classList.add('file--droppable');
 				});
 
 				el.addEventListener('dragleave', function(evt) {
 					evt.stopPropagation();
 					evt.preventDefault();
+
 					el.classList.remove('file--droppable');
 				});
 
 				el.addEventListener('drop', function(evt) {
 					evt.stopPropagation();
+
 					el.classList.remove('file--droppable');
 				});
 			}
+		});
+
+		document.addEventListener('dragover', function(evt) {
+			evt.stopPropagation();
+
+			Array.prototype.forEach.call(element, function(el) {
+				el.classList.add('file--draggable');
+			});
+		});
+
+		document.addEventListener('dragleave', function(evt) {
+			evt.stopPropagation();
+
+			Array.prototype.forEach.call(element, function(el) {
+				el.classList.remove('file--draggable');
+			});
+		});
+
+		document.addEventListener('drop', function(evt) {
+			evt.stopPropagation();
+
+			Array.prototype.forEach.call(element, function(el) {
+				el.classList.remove('file--draggable');
+			});
 		});
 	}
 }
