c1370036bd6ad12a621a0d0423dbbb7ccfef85c7
Maxim Sofronov <imaxsof@gmail.com>
2018-08-17 12:46:49 +0300

    no message

---
+++ src/blocks/page/page.js
@@ -0,0 +1,6 @@
+// Page
+//------------------------------------------------------------------------------
+
+let vh = window.innerHeight * 0.01;
+
+document.querySelector('.page').style.setProperty('--vh', `${vh}px`);
